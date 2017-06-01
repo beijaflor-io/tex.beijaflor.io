b39cf9adb7404eff12597a5e8a32d1a2a1b0b658
yamadapc <tacla.yamada@gmail.com>
2017-05-31 21:44:23 -0300

    Updates

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE QuasiQuotes       #-}
 module Main where
 
-import           Control.Concurrent
 import           Control.Lens
 import           Control.Monad
 import           Control.Monad.Catch
@@ -25,8 +24,10 @@ import           System.Environment
 import           System.Exit
 import           System.IO
 import           System.IO.Temp
-import           System.Process
-import           System.Random
+import           System.Process                (CreateProcess (..),
+                                                StdStream (..), createProcess,
+                                                proc, waitForProcess)
+import           System.Random                 (randomIO)
 import           Text.Blaze.Html.Renderer.Utf8
 import           Text.Hamlet
 import           Web.Spock
@@ -41,6 +42,7 @@ tex args = (proc "context" args) { std_out = NoStream
 curlExample :: Text -> Text
 curlExample host = "curl " <> host <> " -F \"file=@./input.tex\" > output.pdf"
 
+getHome :: MonadIO m => Text -> ActionCtxT ctx m b
 getHome host = preferredFormat >>= \case
     PrefHTML -> lazyBytes $ renderHtml $ [shamlet|
 <html>
@@ -76,95 +78,83 @@ getHome host = preferredFormat >>= \case
 
 runTex :: FilePath -> IO (FilePath, FilePath)
 runTex target = do
-    tmp <- getTemporaryDirectory
-    dir <- createTempDirectory tmp "simple-tex-service"
+  tmp <- getTemporaryDirectory
+  dir <- createTempDirectory tmp "simple-tex-service"
+  let tmpTarget = dir ++ "/target.tex"
+      logPath = dir ++ "/target.log"
+      pdfPath = dir ++ "/target.pdf"
+  copyFileWithMetadata target tmpTarget
+  (Nothing, Nothing, Nothing, ph) <-
+    createProcess ((tex ["--noconsole", tmpTarget]) {cwd = Just dir})
+  ec <- waitForProcess ph
+  case ec of
+    ExitSuccess   -> return ()
+    ExitFailure e -> error ("Context failed with: " ++ show e)
+  return (pdfPath, logPath)
 
-    let tmpTarget = dir ++ "/target.tex"
-        logPath = dir ++ "/target.log"
-        pdfPath = dir ++ "/target.pdf"
-    copyFileWithMetadata target tmpTarget
-    (Nothing, Nothing, Nothing, ph) <- createProcess ((tex ["--noconsole", tmpTarget]) { cwd = Just dir })
-    ec <- waitForProcess ph
-    case ec of
-        ExitSuccess -> return ()
-        ExitFailure e -> error ("Context failed with: " ++ show e)
-    return ( pdfPath
-           , logPath
-           )
-
+run :: AWS.BucketName -> FilePath -> IO (String, String, String)
 run bucketName targetFile = do
-    lgr <- AWS.newLogger AWS.Debug stdout
-    env <- AWS.newEnv AWS.Discover
-    uuid <- UUID.toString <$> randomIO
-    putStrLn $ "Got request " ++ uuid
-
-    f <- show . MD5.md5 <$> ByteStringL.readFile targetFile
-    putStrLn $ "Hash " ++ (show f)
-
-    let pdfS3Key = f ++ ".pdf"
-        logS3Key = f ++ ".log"
-        texS3Key = f ++ ".tex"
-
-    exists <- AWS.runResourceT $ AWS.runAWS (env & AWS.envLogger .~ lgr) $ (do
-        res <- AWS.send (AWS.headObject bucketName (fromString pdfS3Key))
-        return (res ^. AWS.horsResponseStatus == 200)
-      ) `catch` (\(AWS.ServiceError _) -> return False)
-
-    if exists
-        then return (pdfS3Key, logS3Key, texS3Key)
-        else do
-            (pdfFile, logFile) <- runTex targetFile
-            putStrLn $ "Generated files for " ++ uuid
-            putStrLn $ "Terminating response for " ++ uuid
-            pdfBody <- ByteStringL.readFile pdfFile
-            texBody <- ByteStringL.readFile targetFile
-            logBody <- ByteStringL.readFile logFile
-            uploadAllToS3 env lgr [ (pdfS3Key, pdfBody, "application/pdf")
-                                  , (texS3Key, texBody, "text/plain")
-                                  , (logS3Key, logBody, "text/plain")
-                                  ]
-            return (pdfS3Key, logS3Key, texS3Key)
+  lgr <- AWS.newLogger AWS.Debug stdout
+  env <- AWS.newEnv AWS.Discover
+  uuid <- UUID.toString <$> randomIO
+  putStrLn $ "Got request " ++ uuid
+  f <- show . MD5.md5 <$> ByteStringL.readFile targetFile
+  putStrLn $ "Hash " ++ (show f)
+  let pdfS3Key = f ++ ".pdf"
+      logS3Key = f ++ ".log"
+      texS3Key = f ++ ".tex"
+  exists <-
+    AWS.runResourceT $
+    AWS.runAWS (env & AWS.envLogger .~ lgr) $
+    (do res <- AWS.send (AWS.headObject bucketName (fromString pdfS3Key))
+        return (res ^. AWS.horsResponseStatus == 200)) `catch`
+    (\(AWS.ServiceError _) -> return False)
+  if exists
+    then return (pdfS3Key, logS3Key, texS3Key)
+    else do
+      (pdfFile, logFile) <- runTex targetFile
+      putStrLn $ "Generated files for " ++ uuid
+      putStrLn $ "Terminating response for " ++ uuid
+      pdfBody <- ByteStringL.readFile pdfFile
+      texBody <- ByteStringL.readFile targetFile
+      logBody <- ByteStringL.readFile logFile
+      uploadAllToS3
+        env
+        lgr
+        [ (pdfS3Key, pdfBody, "application/pdf")
+        , (texS3Key, texBody, "text/plain")
+        , (logS3Key, logBody, "text/plain")
+        ]
+      return (pdfS3Key, logS3Key, texS3Key)
   where
     uploadAllToS3 env lgr pairs =
-        AWS.runResourceT $ AWS.runAWS (env & AWS.envLogger .~ lgr) $
-            forM_ pairs $ \(k, b, ct) -> uploadToS3 k b ct
+      AWS.runResourceT $
+      AWS.runAWS (env & AWS.envLogger .~ lgr) $
+      forM_ pairs $ \(k, b, ct) -> uploadToS3 k b ct
     uploadToS3 key file contentType = do
-        let req = AWS.putObject bucketName (fromString key) (AWS.toBody file)
-            req' = req & AWS.poContentType .~ (Just contentType)
-        AWS.send req'
+      let req = AWS.putObject bucketName (fromString key) (AWS.toBody file)
+          req' = req & AWS.poContentType .~ (Just contentType)
+      AWS.send req'
 
 main :: IO ()
 main = do
-    host <- getEnv "STS_HOST"
-    bucketName <- getEnv "STS_BUCKET_NAME"
-    port <- read . fromMaybe "3003" <$> lookupEnv "PORT"
-    spockCfg <- defaultSpockCfg () PCNoDatabase ()
-    runSpock port $ spock spockCfg $ do
-        get "/" (getHome host)
-        post "/" $ do
-            fs <- files
-            let Just targetFile = uf_tempLocation <$> (HashMap.lookup "file" fs)
-            liftIO $ print targetFile
-            (pdfS3Key, logS3Key, texS3Key) <- liftIO $ run bucketName targetFile
-            let setHeader' h = setHeader h . fromString
-            setHeader' "x-sts-pdf" (s3Url bucketName pdfS3Key)
-            setHeader' "x-sts-log" (s3Url bucketName logS3Key)
-            setHeader' "x-sts-tex" (s3Url bucketName texS3Key)
-            redirect $ fromString (s3Url pdfS3Key)
+  host <- Text.pack <$> getEnv "STS_HOST"
+  bucketName <- AWS.BucketName . Text.pack <$> getEnv "STS_BUCKET_NAME"
+  port <- read . fromMaybe "3003" <$> lookupEnv "PORT"
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock port $
+    spock spockCfg $ do
+      get "/" (getHome host)
+      post "/" $ do
+        fs <- files
+        let Just targetFile = uf_tempLocation <$> HashMap.lookup "file" fs
+        liftIO $ print targetFile
+        (pdfS3Key, logS3Key, texS3Key) <- liftIO $ run bucketName targetFile
+        let setHeader' h = setHeader h . fromString
+        setHeader' "x-sts-pdf" (s3Url bucketName pdfS3Key)
+        setHeader' "x-sts-log" (s3Url bucketName logS3Key)
+        setHeader' "x-sts-tex" (s3Url bucketName texS3Key)
+        redirect $ fromString (s3Url bucketName pdfS3Key)
   where
-    s3Url bucketName u = "https://" <> bucketName <> ".s3.amazonaws.com/" <> u
-
-{-
-(Nothing, Just inp, Just err, ph) <- createProcess (tex ["./examples/test.tex"])
-let runner label outputHandle inputHandle = do
-        isEOF <- hIsEOF inputHandle
-        unless isEOF $ do
-            ln <- hGetLine inputHandle
-            hPutStrLn outputHandle ("[" ++ label ++ "] " ++ ln)
-            runner label outputHandle inputHandle
-            hFlush stdout
-_ <- forkIO $ runner "stdout" stdout inp
-_ <- forkIO $ runner "stderr" stderr err
-ec <- waitForProcess ph
-print ec
--}
+    s3Url (AWS.BucketName bucketName) u =
+      "https://" <> Text.unpack bucketName <> ".s3.amazonaws.com/" <> u
